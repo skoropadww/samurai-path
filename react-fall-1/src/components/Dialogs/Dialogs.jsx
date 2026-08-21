b4d4180377600fc1c84a5ba174e8f23d8879f913
Vitalii <skoropadww@email.com>
2026-08-21 09:28:14 +0300

    BLL and UI object data

--- react-fall-1/src/components/Dialogs/Dialogs.jsx
+++ react-fall-1/src/components/Dialogs/Dialogs.jsx
@@ -4,26 +4,28 @@ import DialogItem from './DialogItem/DialogItem'
 import Message from './Message/Message'
 
 const Dialogs = (props) => {
-  let dialogs = [
+  let dialogsData = [
     { id: 1, name: 'Dimuchka' },
     { id: 2, name: 'Vilat' },
     { id: 3, name: 'Lida' },
     { id: 4, name: 'Vasia' },
     { id: 5, name: 'Pasha' },
   ]
+  let messagesData = [
+    { id: 1, message: 'Hello' },
+    { id: 2, message: 'How are you?' },
+    { id: 3, message: "I'm fine, thank you!" },
+    { id: 4, message: 'What are you doing?' },
+    { id: 5, message: 'I\'m doing nothing, you?' },
+    { id: 6, message: 'I\'m doing nothing, you?' },
+  ]
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialog}>
-        {dialogs.map(dialog => <DialogItem key={dialog.id} name={dialog.name} id={dialog.id} />)}
+        {dialogsData.map(dialog => <DialogItem key={dialog.id} name={dialog.name} id={dialog.id} />)}
       </div>
       <div className={classes.messages}>
-        <Message message="Hello" />
-        <Message message="How are you?" />
-        <Message message="I'm fine, thank you!" />
-        <Message message="What are you doing?" />
-        <Message message="I'm doing nothing, you?" />
-        <Message message="I'm doing nothing, you?" />
-        <Message message="I'm doing nothing, you?" />
+        {messagesData.map(message => <Message key={message.id} message={message.message} />)}
       </div>
     </div>
   )
--- react-fall-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-fall-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,11 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  let postsData = [
+    { id: 1, massege: 'Hello, world!', likeCount: 10 },
+    { id: 2, massege: 'Hi, how are you?', likeCount: 20 },
+    { id: 3, massege: 'I am fine, thank you!', likeCount: 30 },
+  ]
   return (
     <div className={classes.news}>
         <div className={classes.news_title}>
@@ -13,12 +18,10 @@ const MyPosts = () => {
             <button type='submit'>New post</button>
         </form>
         <div className={classes.news_posts}>
-            <Post massege='Hello, world!' likeCount={10}/>
-            <Post massege='Hi, how are you?' likeCount={20}/>
-            <Post massege='I am fine, thank you!' likeCount={30}/>
+            {postsData.map(post => <Post key={post.id} massege={post.massege} likeCount={post.likeCount}/>)}
         </div>
     </div>
-  );
-};
+  )
+}
 
-export default MyPosts;
+export default MyPosts
