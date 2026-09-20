4c4e399ee9039afebacba5ced8827f7416cf5896
Vitalii <skoropadww@email.com>
2026-09-20 10:48:32 +0300

    53 Users Class Component

--- react-fall-1/src/components/Users/Users.jsx
+++ react-fall-1/src/components/Users/Users.jsx
@@ -2,69 +2,70 @@ import React from 'react'
 import s from './Users.module.css'
 import axios from 'axios'
 
-const Users = (props) => {
-  let getUsers = () => {
-    if (props.users.length === 0) {
+class Users extends React.Component {
+  getUsers = () => {
+    if (this.props.users.length === 0) {
       axios
         .get('/samurai-api/api/1.0/users', {
           withCredentials: true,
         })
         .then((response) => {
-          props.setUsers(response.data.items)
+          this.props.setUsers(response.data.items)
         })
     }
   }
-
-  return (
-    <div className={s.users}>
-      <button onClick={getUsers}>Get Users</button>
-      <h1 className={s.title}>Users</h1>
-      <div className={s.list}>
-        {props.users.map((u) => (
-          <div key={u.id} className={s.user_wrap}>
-            <div className={s.user_left}>
-              <div className={s.user_photo}>
-                <img
-                  src={
-                    u.photos.small != null
-                      ? u.photos.small
-                      : 'https://i.pravatar.cc/150?u=' + u.id
-                  }
-                  alt={u.name}
-                />
-              </div>
-              {u.followed ? (
-                <button
-                  className={s.user_btn_follow}
-                  onClick={() => props.unfollow(u.id)}
-                >
-                  Unfollow
-                </button>
-              ) : (
-                <button
-                  className={s.user_btn_follow}
-                  onClick={() => props.follow(u.id)}
-                >
-                  Follow
-                </button>
-              )}
-            </div>
-
-            <div className={s.user_right}>
-              <div className={s.user_right_top}>
-                <span className={s.user_name}>{u.name}</span>
-                <div className={s.user_location}>
-                  <span className={s.user_country}>{'country'}</span>
-                  <span className={s.user_city}>{'city'}</span>
+  render() {
+    return (
+      <div className={s.users}>
+        <button onClick={this.getUsers}>Get Users</button>
+        <h1 className={s.title}>Users</h1>
+        <div className={s.list}>
+          {this.props.users.map((u) => (
+            <div key={u.id} className={s.user_wrap}>
+              <div className={s.user_left}>
+                <div className={s.user_photo}>
+                  <img
+                    src={
+                      u.photos.small != null
+                        ? u.photos.small
+                        : 'https://i.pravatar.cc/150?u=' + u.id
+                    }
+                    alt={u.name}
+                  />
                 </div>
+                {u.followed ? (
+                  <button
+                    className={s.user_btn_follow}
+                    onClick={() => this.props.unfollow(u.id)}
+                  >
+                    Unfollow
+                  </button>
+                ) : (
+                  <button
+                    className={s.user_btn_follow}
+                    onClick={() => this.props.follow(u.id)}
+                  >
+                    Follow
+                  </button>
+                )}
               </div>
-              <div className={s.user_status}>{u.status}</div>
+
+              <div className={s.user_right}>
+                <div className={s.user_right_top}>
+                  <span className={s.user_name}>{u.name}</span>
+                  <div className={s.user_location}>
+                    <span className={s.user_country}>{'country'}</span>
+                    <span className={s.user_city}>{'city'}</span>
+                  </div>
+                </div>
+                <div className={s.user_status}>{u.status}</div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Users
