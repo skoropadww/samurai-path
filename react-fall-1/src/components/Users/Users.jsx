4ea4edd6d1d3e562b502d56af69e7625d8ce0943
Vitalii <skoropadww@email.com>
2026-09-22 09:26:51 +0300

    56 recode UsersAPIContainer Container component

--- react-fall-1/src/components/Users/Users.jsx
+++ react-fall-1/src/components/Users/Users.jsx
@@ -1,90 +1,126 @@
 import React from 'react'
 import s from './Users.module.css'
-import axios from 'axios'
 
-class Users extends React.Component {
-  componentDidMount() {
-    axios.get(`/samurai-api/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-      withCredentials: true,
-    }).then((response) => {
-      this.props.setUsers(response.data.items)
-      console.log(response.data)
-      this.props.setTotalUsersCount(response.data.totalCount)
-    })
+const Users = (props) => {
+  let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize) || 1
+  let pages = []
+
+  // показываем только «окно» из 10 страниц вокруг текущей
+  const portionSize = 10
+  let leftPage = Math.max(1, props.currentPage - Math.floor(portionSize / 2))
+  let rightPage = leftPage + portionSize - 1
+
+  if (rightPage > pagesCount) {
+    rightPage = pagesCount
+    leftPage = Math.max(1, rightPage - portionSize + 1)
   }
 
-  onPageChanged = (pageNumber) => {
-    this.props.setCurrentPage(pageNumber)
-    axios.get(
-      `/samurai-api/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
-      { withCredentials: true }
-    ).then((response) => {
-      this.props.setUsers(response.data.items)
-    })
+  for (let i = leftPage; i <= rightPage; i++) {
+    pages.push(i)
   }
 
-  render() {
+  return (
+    <div className={s.users}>
+      <h1 className={s.title}>Users</h1>
+      <div className={s.pagination}>
+        {props.currentPage > 1 && (
+          <span
+            className={s.pageNav}
+            onClick={() => props.onPageChanged(props.currentPage - 1)}
+          >
+            ←
+          </span>
+        )}
 
-    let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-    let pages = []
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i)
-    }
-    return (
-      <div className={s.users}>
-        <h1 className={s.title}>Users</h1>
-          <div className={s.pagination}>
-            {pages.map((p) => (
-              <span key={p} className={this.props.currentPage === p ? s.selectedPage : ''} onClick={ ()=> {this.onPageChanged(p)}}>{p}</span>
-            ))}
-          </div>
-        <div className={s.list}>
-          {this.props.users.map((u) => (
-            <div key={u.id} className={s.user_wrap}>
-              <div className={s.user_left}>
-                <div className={s.user_photo}>
-                  <img
-                    src={
-                      u.photos.small != null
-                        ? u.photos.small
-                        : 'https://i.pravatar.cc/150?u=' + u.id
-                    }
-                    alt={u.name}
-                  />
-                </div>
-                {u.followed ? (
-                  <button
-                    className={s.user_btn_follow}
-                    onClick={() => this.props.unfollow(u.id)}
-                  >
-                    Unfollow
-                  </button>
-                ) : (
-                  <button
-                    className={s.user_btn_follow}
-                    onClick={() => this.props.follow(u.id)}
-                  >
-                    Follow
-                  </button>
-                )}
-              </div>
+        {leftPage > 1 && (
+          <>
+            <span
+              className={props.currentPage === 1 ? s.selectedPage : ''}
+              onClick={() => props.onPageChanged(1)}
+            >
+              1
+            </span>
+            {leftPage > 2 && <span className={s.pageDots}>...</span>}
+          </>
+        )}
 
-              <div className={s.user_right}>
-                <div className={s.user_right_top}>
-                  <span className={s.user_name}>{u.name}</span>
-                  <div className={s.user_location}>
-                    <span className={s.user_country}>{'country'}</span>
-                    <span className={s.user_city}>{'city'}</span>
-                  </div>
-                </div>
-                <div className={s.user_status}>{u.status}</div>
-              </div>
-            </div>
-          ))}
-        </div>
+        {pages.map((p) => (
+          <span
+            key={p}
+            className={props.currentPage === p ? s.selectedPage : ''}
+            onClick={() => props.onPageChanged(p)}
+          >
+            {p}
+          </span>
+        ))}
+
+        {rightPage < pagesCount && (
+          <>
+            {rightPage < pagesCount - 1 && <span className={s.pageDots}>...</span>}
+            <span
+              className={props.currentPage === pagesCount ? s.selectedPage : ''}
+              onClick={() => props.onPageChanged(pagesCount)}
+            >
+              {pagesCount}
+            </span>
+          </>
+        )}
+
+        {props.currentPage < pagesCount && (
+          <span
+            className={s.pageNav}
+            onClick={() => props.onPageChanged(props.currentPage + 1)}
+          >
+            →
+          </span>
+        )}
       </div>
-    )
-  }
+      <div className={s.list}>
+        {props.users.map((u) => (
+          <div key={u.id} className={s.user_wrap}>
+            <div className={s.user_left}>
+              <div className={s.user_photo}>
+                <img
+                  src={
+                    u.photos.small != null
+                      ? u.photos.small
+                      : 'https://i.pravatar.cc/150?u=' + u.id
+                  }
+                  alt={u.name}
+                />
+              </div>
+              {u.followed ? (
+                <button
+                  className={s.user_btn_follow}
+                  onClick={() => props.unfollow(u.id)}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  className={s.user_btn_follow}
+                  onClick={() => props.follow(u.id)}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+
+            <div className={s.user_right}>
+              <div className={s.user_right_top}>
+                <span className={s.user_name}>{u.name}</span>
+                <div className={s.user_location}>
+                  <span className={s.user_country}>{'country'}</span>
+                  <span className={s.user_city}>{'city'}</span>
+                </div>
+              </div>
+              <div className={s.user_status}>{u.status}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Users
--- react-fall-1/src/components/Users/UsersContainer.jsx
+++ react-fall-1/src/components/Users/UsersContainer.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
-import Users from './Users'
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from '../../redux/users-reducer'
 import { connect } from 'react-redux'
+import axios from 'axios'
+import Users from './Users'
+
+class UsersContainerClass extends React.Component {
+    componentDidMount() {
+      axios.get(`/samurai-api/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
+        withCredentials: true,
+      }).then((response) => {
+        this.props.setUsers(response.data.items)
+        this.props.setTotalUsersCount(response.data.totalCount)
+      })
+    }
+  
+    onPageChanged = (pageNumber) => {
+      this.props.setCurrentPage(pageNumber)
+      axios.get(
+        `/samurai-api/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+        { withCredentials: true }
+      ).then((response) => {
+        this.props.setUsers(response.data.items)
+      })
+    }
+  
+    render() {
+      return (
+        <Users 
+          totalUsersCount={this.props.totalUsersCount} 
+          pageSize={this.props.pageSize} 
+          currentPage={this.props.currentPage} 
+          users={this.props.users} 
+          onPageChanged={this.onPageChanged} 
+          follow={this.props.follow} 
+          unfollow={this.props.unfollow} 
+        />
+      )
+    }
+  }
 
 let mapStateToProps = (state) => {
     return {
@@ -31,6 +67,6 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersContainerClass)
 
 export default UsersContainer
